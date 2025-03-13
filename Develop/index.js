cb8bc410c7cf6474a05a0195471129e36e83f2c9
dalya kablawi <dalyakablawi@gmail.com>
2025-03-13 11:18:39 -0400

    changes >

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirerPkg = require('inquirer').default;
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
